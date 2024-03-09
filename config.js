cb245a29bc395f42c5a1bbff53c07e9c1d8cf15b
Zykov Anton <anton.zykov1@gmail.com>
2024-03-09 01:12:13 +0100

    Initial commit 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	clientID: '774d4ad1ea7014f2',
 	clientSecret: '8d3a3ec96dc2c587a4807137eefcf71131f0de73',
-	callbackURL: 'http://localhost:3000/auth/pipedrive/callback'
+	callbackURL: 'https://fiftyfifty.onrender.com/auth/pipedrive/callback'
 };
